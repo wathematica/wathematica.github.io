5280d3726ea154cae1434222f177f5d30d5de8a8
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-02-06 00:25:39 +0900

    ハンバーガー自体のアニメーションを追加した

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
   "use strict";
 
   window.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".hamburger").addEventListener("click", () => {
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("is-active");
       document.querySelector(".header__navigation").classList.toggle("is-open");
     });
   });
